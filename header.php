09fde7493037e88e4507b91ff735499f3519cd42
PetarTosic <petar94bs@gmail.com>
2023-02-24 16:53:40 +0100

    Added patients list

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <ul>
     <li style="float:left"><a href="index.php">HOME</a></li>
-    <li style="float:right"><a href="doktori.php">Pacijenti</a></li>
+    <li style="float:right"><a href="pacijenti.php">Pacijenti</a></li>
     <li style="float:right"><a href="doktori.php">Doktori</a></li>
 </ul>
--- pacijenti.php
+++ pacijenti.php
@@ -8,25 +8,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Doktori</title>
+    <title>Pacijenti</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php include("header.php"); 
-    $sql = "SELECT * FROM doktori";
+    $sql = "SELECT * FROM pacijenti";
     $statement = $connection->prepare($sql);
     $statement->execute();
     $statement->setFetchMode(PDO::FETCH_ASSOC);
-    $doktori = $statement->fetchAll();
+    $pacijenti = $statement->fetchAll();
     ?>
 
-    <?php foreach($doktori AS $doktor) { ?>
+    <?php foreach($pacijenti AS $pacijent) { ?>
         <div class="dok-div">
-            <p class="dok-p"><?php echo "Ime doktora: " . $doktor['ime'] . " " . $doktor['prezime']; ?></p>
-            <p class="dok-p"><?php echo "Email: " . $doktor['email']; ?></p>
+            <p class="dok-p"><?php echo "Ime pacijenta: " . $pacijent['ime'] . " " . $pacijent['prezime']; ?></p>
+            <p class="dok-p"><?php echo "Email: " . $pacijent['email']; ?></p>
         </div>
     <?php } ?>
-    <a class="dok-button" href="nov-doktor.php">Unesi novog doktora</a><br>
-    <a class="dok-button" href="nov-pacijent.php">Unesi novog pacijenta</a>
+    
 </body>
 </html>
