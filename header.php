52c48679332460c2a824bc3b1595ba7ac304e850
PetarTosic <petar94bs@gmail.com>
2023-02-27 18:40:01 +0100

    Test

--- header.php
+++ header.php
@@ -1,5 +1,20 @@
-<ul>
-    <li style="float:left"><a href="index.php">HOME</a></li>
-    <li style="float:right"><a href="pacijenti.php">Pacijenti</a></li>
-    <li style="float:right"><a href="doktori.php">Doktori</a></li>
-</ul>
+<header>
+    <div class="blog-masthead">
+        <div class="container">
+            <nav class="nav">
+                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link" href="#">New features</a>
+                <a class="nav-link" href="#">Press</a>
+                <a class="nav-link" href="#">New hires</a>
+                <a class="nav-link" href="#">About</a>
+            </nav>
+        </div>
+    </div>
+
+    <div class="blog-header">
+        <div class="container">
+            <h1 class="blog-title">The Bootstrap Blog</h1>
+            <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+        </div>
+    </div>
+</header>
